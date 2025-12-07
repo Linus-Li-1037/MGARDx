55432958ce4942882faecd4613a45c4b5fcd90f6
Anonymous <anon@eurosys.org>
2025-12-07 01:05:09 -0500

    adding new interpolation

--- install/include/decompose_interleave_new.hpp
+++ install/include/decompose_interleave_new.hpp
@@ -1,5 +1,5 @@
-#ifndef _MGARD_DECOMPOSE_INTERLEAVE_HPP
-#define _MGARD_DECOMPOSE_INTERLEAVE_HPP
+#ifndef _MGARD_DECOMPOSE_INTERLEAVE_NEW_HPP
+#define _MGARD_DECOMPOSE_INTERLEAVE_NEW_HPP
 
 #include <vector>
 #include <cstdlib>
@@ -14,12 +14,12 @@ namespace MGARD{
 using namespace std;
 
 template <class T>
-class Decomposer_Interleaver{
+class Decomposer_Interleaver_new{
 public:
-	Decomposer_Interleaver(bool use_sz_=true){
+	Decomposer_Interleaver_new(bool use_sz_=true){
             use_sz = use_sz_;
         };
-	~Decomposer_Interleaver(){
+	~Decomposer_Interleaver_new(){
 	};
     // return decomposed and interleaved data buffers for each level, num_levels = Dims * MGARD_level (target_level)
 	// Combining MGARD Decomposer and Interleaver
@@ -67,15 +67,14 @@ public:
 
 private:
     bool use_sz = true;
-	std::vector<std::vector<uint32_t>> level_dims;
-	std::vector<std::vector<uint32_t>> level_buffer_dims;
 	std::vector<std::vector<T>> level_buffers;
+	std::vector<std::vector<uint32_t>> level_buffer_dims;
 	std::vector<uint32_t> level_sizes;
 	T * data_begin;
 
 	void init(const vector<size_t>& dims, size_t target_level){
 		std::vector<uint32_t> dims_uint32(dims.begin(), dims.end());
-		level_dims = compute_level_dims_new(dims_uint32, target_level);
+		auto level_dims = compute_level_dims_new(dims_uint32, target_level);
 		level_sizes = compute_level_buffers_size(level_dims, target_level, level_buffer_dims);
 		for(int i=0; i<level_sizes.size(); i++){
 			level_buffers.push_back(std::vector<T>(level_sizes[i]));
@@ -97,38 +96,13 @@ private:
 		for(i=1; i+1<n; i+=2){
 			size_t c = begin + i * stride;
 			// buffer[c] -= (buffer[c - stride] + buffer[c + stride]) / 2;
-			// std::cout << (&data[c] - data_begin) << " -= (" << (&data[c - stride] - data_begin) << " + " << (&data[c + stride] - data_begin) << ")/2" << std::endl;
+			// if((&data[c] - data_begin) == 2) std::cout << (&data[c] - data_begin) << " -= (" << (&data[c - stride] - data_begin) << " + " << (&data[c + stride] - data_begin) << ")/2" << std::endl;
 			buffer[count++] = data[c] - (data[c - stride] + data[c + stride]) / 2;
-			data[c] = (data[c - stride] + data[c + stride]) / 2;
 		}
 		if(n % 2 == 0){
 			size_t c = begin + (n - 1) * stride;
-			// std::cout << (&data[c] - data_begin) << " -= " << (&data[c - stride] - data_begin) << std::endl;
 			// buffer[c] -= buffer[c - stride];
 			buffer[count++] = data[c] - data[c - stride];
-			data[c] = data[c - stride];
-		}
-		return count;
-	}
-
-	size_t compute_interpolant_difference_1D_diff_direct(const size_t begin, const size_t end, const size_t stride, const size_t interp_stride, bool last_even, T * data, T * buffer){
-		size_t n = (end - begin) / stride + 1;
-		size_t i = 0;
-		size_t count = 0;
-		if(!last_even){
-			for(i=0; i<n; i+=2){
-				size_t c = begin + i * stride;
-				// std::cout << (&data[c] - data_begin) << " -= (" << (&data[c - interp_stride] - data_begin) << " + " << (&data[c + interp_stride] - data_begin) << ")/2" << std::endl;
-				buffer[count++] = data[c] - (data[c - interp_stride] + data[c + interp_stride]) / 2;
-				data[c] = (data[c - interp_stride] + data[c + interp_stride]) / 2;
-			}
-		} else {
-			for(i=0; i<n; i+=2){
-				size_t c = begin + i * stride;
-				// std::cout << (&data[c] - data_begin) << " -= " << (&data[c - interp_stride] - data_begin) << std::endl;
-				buffer[count++] = data[c] - data[c - interp_stride];
-				data[c] = data[c - interp_stride];
-			}
 		}
 		return count;
 	}
@@ -165,45 +139,8 @@ private:
 		size_t stride_n1 = n2 * h;
 		size_t stride_n2 = h;
 
-		T * cur_data_pos;
-		T * cur_buffer_pos;
-
-		// old implementation: slow
-		// cur_data_pos = data_pos;
-		// cur_buffer_pos = level_buffers[((current_level - 1) * 2) + 1].data();
-		// h = h << 1;
-		// size_t n1_begin = 0;
-		// size_t n1_end = (n1 - 1) * n2;
-		// for(size_t i=0; i<n2; i+=h){
-		// 	count = compute_interpolant_difference_1D(n1_begin, n1_end, stride_n1, cur_data_pos, cur_buffer_pos);
-		// 	cur_data_pos += h;
-		// 	cur_buffer_pos += count;
-		// 	count_2D += count;
-		// }
-
-		// new implemenation: fast
-		cur_data_pos = data_pos + stride_n1;
-		cur_buffer_pos = level_buffers[((current_level - 1) * 2) + 1].data();
-		// h = h << 1;
-		size_t n1_begin = 0;
-		size_t n1_end = (n1 - 1) * n2;
-		size_t new_n1 = (n1_end - n1_begin) / stride_n1 + 1;
-		size_t cur_begin = 0;
-		size_t cur_end = n2 - 1;
-		for(size_t i=1; i+1<new_n1; i+=2){
-			count = compute_interpolant_difference_1D_diff_direct(cur_begin, cur_end, h, stride_n1, false, cur_data_pos, cur_buffer_pos);
-			cur_data_pos += 2 * stride_n1;
-			cur_buffer_pos += count;
-			count_2D += count;
-		}
-		if(new_n1 % 2 == 0){
-			count = compute_interpolant_difference_1D_diff_direct(cur_begin, cur_end, h, stride_n1, true, cur_data_pos, cur_buffer_pos);
-			count_2D += count;
-		}
-		
-		// h = h >> 1;
-		cur_data_pos = data_pos;
-		cur_buffer_pos = level_buffers[((current_level - 1) * 2) + 2].data();
+		T * cur_data_pos = data_pos;
+		T * cur_buffer_pos = level_buffers[((current_level - 1) * 2) + 2].data();
 		size_t n2_begin = 0;
 		size_t n2_end = n2 - 1;
 		for(size_t i=0; i<n1; i+=h){
@@ -212,7 +149,18 @@ private:
 			cur_buffer_pos += count;
 			count_2D += count;
 		}
-		
+		// compute vertical difference
+		cur_data_pos = data_pos;
+		cur_buffer_pos = level_buffers[((current_level - 1) * 2) + 1].data();
+		h = h << 1;
+		size_t n1_begin = 0;
+		size_t n1_end = (n1 - 1) * n2;
+		for(size_t i=0; i<n2; i+=h){
+			count = compute_interpolant_difference_1D(n1_begin, n1_end, stride_n1, cur_data_pos, cur_buffer_pos);
+			cur_data_pos += h;
+			cur_buffer_pos += count;
+			count_2D += count;
+		}
 		return count_2D;
 	}
 
@@ -253,99 +201,12 @@ private:
 		size_t count;
 		size_t stride_n1 = n2 * n3 * h;
 		size_t stride_n2 = n3 * h;
-		size_t stride_n3 = h;
 
-		T * cur_data_pos;
-		T * temp_data_pos;
-		T * cur_buffer_pos;
-		size_t h2x = h << 1;
-
-		size_t n1_begin = 0;
-		size_t n1_end = (n1-1) * n2 * n3;
-		size_t new_n1 = (n1_end - n1_begin) / stride_n1 + 1;
-		size_t n2_begin = 0;
-		size_t n2_end = (n2 - 1) * n3;
-		size_t new_n2 = (n2_end - n2_begin) / stride_n2 + 1;
+		T * cur_data_pos = data_pos;
+		T * temp_data_pos = cur_data_pos;
+		T * cur_buffer_pos = level_buffers[((current_level - 1) * 3) + 3].data();
 		size_t n3_begin = 0;
 		size_t n3_end = n3 - 1;
-		size_t new_n3 = (n3_end - n3_begin) / stride_n3 + 1;
-		
-
-		// old implementation: slow
-		// cur_data_pos = data_pos;
-		// cur_buffer_pos = level_buffers[((current_level - 1) * 3) + 1].data();
-		// size_t n1_begin = 0;
-		// size_t n1_end = (n1-1) * n2 * n3;
-		// for(size_t i=0; i<n2; i+=h2x){
-		// 	temp_data_pos = cur_data_pos;
-		// 	for(size_t j=0; j<n3; j+=h2x){
-		// 		count = compute_interpolant_difference_1D(n1_begin, n1_end, stride_n1, temp_data_pos, cur_buffer_pos);
-		// 		temp_data_pos += h2x;
-		// 		cur_buffer_pos += count;
-		// 		count_3D += count;
-		// 	}
-		// 	cur_data_pos += n3 * h2x;
-		// }
-
-		// cur_data_pos = data_pos;
-		// cur_buffer_pos = level_buffers[((current_level - 1) * 3) + 2].data();
-		// size_t n2_begin = 0;
-		// size_t n2_end = (n2-1) * n3;
-		// for(size_t i=0; i<n1; i+=h){
-		// 	temp_data_pos = cur_data_pos;
-		// 	for(size_t j=0; j<n3; j+=h2x){
-		// 		count = compute_interpolant_difference_1D(n2_begin, n2_end, stride_n2, temp_data_pos, cur_buffer_pos);
-		// 		temp_data_pos += h2x;
-		// 		cur_buffer_pos += count;
-		// 		count_3D += count;
-		// 	}
-		// 	cur_data_pos += n2*n3*h;
-		// }
-
-		// new implementation: fast
-		cur_data_pos = data_pos + stride_n1;
-		cur_buffer_pos = level_buffers[((current_level - 1) * 3) + 1].data();
-		for(size_t i=1; i+1<new_n1; i+=2){
-			temp_data_pos = cur_data_pos;
-			for(size_t j=0; j<n2; j+=h2x){
-				count = compute_interpolant_difference_1D_diff_direct(n3_begin, n3_end, h, stride_n1, false, temp_data_pos, cur_buffer_pos);
-				temp_data_pos += 2 * stride_n2;
-				cur_buffer_pos += count;
-				count_3D += count;
-			}
-			cur_data_pos += 2 * stride_n1;
-		}
-		if(new_n1 % 2 == 0){
-			temp_data_pos = cur_data_pos;
-			for(size_t j=0; j<n2; j+=h2x){
-				count = compute_interpolant_difference_1D_diff_direct(n3_begin, n3_end, h, stride_n1, true, temp_data_pos, cur_buffer_pos);
-				temp_data_pos += 2 * stride_n2;
-				cur_buffer_pos += count;
-				count_3D += count;
-			}
-		}
-
-		cur_data_pos = data_pos + stride_n2;
-		cur_buffer_pos = level_buffers[((current_level - 1) * 3) + 2].data();
-		for(size_t i=0; i<n1; i+=h){
-			temp_data_pos = cur_data_pos;
-			for(size_t j=1; j+1<new_n2; j+=2){
-				count = compute_interpolant_difference_1D_diff_direct(n3_begin, n3_end, h, stride_n2, false, temp_data_pos, cur_buffer_pos);
-				temp_data_pos += 2 * stride_n2;
-				cur_buffer_pos += count;
-				count_3D += count;
-			}
-			if(new_n2 % 2 == 0){
-				count = compute_interpolant_difference_1D_diff_direct(n3_begin, n3_end, h, stride_n2, true, temp_data_pos, cur_buffer_pos);
-				cur_buffer_pos += count;
-				count_3D += count;
-			}
-			cur_data_pos += stride_n1;
-		}
-		
-
-		cur_data_pos = data_pos;
-		cur_buffer_pos = level_buffers[((current_level - 1) * 3) + 3].data();
 		for(size_t i=0; i<n1; i+=h){
 			temp_data_pos = cur_data_pos;
 			for(size_t j=0; j<n2; j+=h){
@@ -356,10 +217,42 @@ private:
 			}
 			cur_data_pos += n2*n3*h;
 		}
+
+		size_t h2x = h << 1;
+		cur_data_pos = data_pos;
+		cur_buffer_pos = level_buffers[((current_level - 1) * 3) + 2].data();
+		size_t n2_begin = 0;
+		size_t n2_end = (n2-1) * n3;
+		for(size_t i=0; i<n1; i+=h){
+			temp_data_pos = cur_data_pos;
+			for(size_t j=0; j<n3; j+=h2x){
+				count = compute_interpolant_difference_1D(n2_begin, n2_end, stride_n2, temp_data_pos, cur_buffer_pos);
+				temp_data_pos += h2x;
+				cur_buffer_pos += count;
+				count_3D += count;
+			}
+			cur_data_pos += n2*n3*h;
+		}
+
+		// h = h << 1;
+		cur_data_pos = data_pos;
+		cur_buffer_pos = level_buffers[((current_level - 1) * 3) + 1].data();
+		size_t n1_begin = 0;
+		size_t n1_end = (n1-1) * n2 * n3;
+		for(size_t i=0; i<n2; i+=h2x){
+			temp_data_pos = cur_data_pos;
+			for(size_t j=0; j<n3; j+=h2x){
+				count = compute_interpolant_difference_1D(n1_begin, n1_end, stride_n1, temp_data_pos, cur_buffer_pos);
+				temp_data_pos += h2x;
+				cur_buffer_pos += count;
+				count_3D += count;
+			}
+			cur_data_pos += n3 * h2x;
+		}
 		return count_3D;
 	}
 
-	void decompose_interleave_3D_with_hierarchical_basis(T * data_pos, size_t n1, size_t n2, size_t n3, size_t h, size_t current_level){
+	void decompose_interleave_3D_with_hierarchical_basis(T * data_pos, size_t n1, size_t n2, size_t n3, T h, size_t current_level){
 		size_t count_3D = 0;
         count_3D = (current_level != 0) ? compute_interpolant_difference_3D(data_pos, n1, n2, n3, h, current_level) : interleave_3D_level_0(data_pos, n1, n2, n3, h);
 		assert(count_3D == (current_level) ? level_sizes[((current_level - 1) * 3) + 1] + level_sizes[((current_level - 1) * 3) + 2] + level_sizes[((current_level - 1) * 3) + 3] : level_sizes[0]);
---
+++ test/test_coeff_decompose_interleave.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include <iomanip>
+#include <cmath>
+#include "coeff_decompose_interleave.hpp"
+#include "coeff_reposition_recompose.hpp"
+
+using namespace std;
+
+template <class T>
+std::vector<std::vector<T>> test_decompose(vector<T>& data, const vector<size_t>& dims, int direction, int target_level){
+    struct timespec start, end;
+    int err = 0;
+    err = clock_gettime(CLOCK_REALTIME, &start);
+    MGARD::Coeff_Decomposer_Interleaver<T> decomposer;
+    std::vector<std::vector<T>> level_buffers = decomposer.decompose(data.data(), dims, direction, target_level, true);
+    err = clock_gettime(CLOCK_REALTIME, &end);
+    cout << "Decomposition time: " << (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec)/(double)1000000000 << "s" << endl;
+    return level_buffers;
+}
+
+template <class T>
+std::vector<T> test_recompose(std::vector<std::vector<T>>& level_buffers, const vector<size_t>& dims, int direction, int target_level){
+    struct timespec start, end;
+    int err = 0;
+    err = clock_gettime(CLOCK_REALTIME, &start);
+    MGARD::Coeff_Repositioner_Recomposer<T> recomposer;
+    std::vector<T> recovered_data = recomposer.recompose(level_buffers, dims, direction, target_level, true);
+    err = clock_gettime(CLOCK_REALTIME, &end);
+    cout << "Recomposition time: " << (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec)/(double)1000000000 << "s" << endl;
+    return recovered_data;
+}
+
+template <class T>
+void test(string filename, const vector<size_t>& dims, int direction, int target_level){
+    size_t num_elements = 0;
+    auto data = MGARD::readfile<T>(filename.c_str(), num_elements);
+    auto data_ori(data);
+    auto level_buffers = test_decompose(data, dims, direction, target_level);
+    auto recovered_data = test_recompose(level_buffers, dims, direction, target_level);
+    // struct timespec start, end;
+    // int err = 0;
+    // err = clock_gettime(CLOCK_REALTIME, &start);
+    // MGARD::Decomposer_Interleaver<T> decomposer;
+    // std::vector<std::vector<T>> level_buffers = decomposer.decompose(data.data(), dims, target_level, true);
+    // err = clock_gettime(CLOCK_REALTIME, &end);
+    // cout << "Decomposition time: " << (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec)/(double)1000000000 << "s" << endl;
+    // err = clock_gettime(CLOCK_REALTIME, &start);
+    // MGARD::Repositioner_Recomposer<T> recomposer;
+    // std::vector<T> recovered_data = recomposer.recompose(level_buffers, dims, target_level, true);
+    // err = clock_gettime(CLOCK_REALTIME, &end);
+    // cout << "Recomposition time: " << (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec)/(double)1000000000 << "s" << endl;
+    MGARD::print_statistics(data_ori.data(), recovered_data.data(), num_elements);
+}
+
+int main(int argc, char ** argv){
+    string filename = string(argv[1]);
+    int type = atoi(argv[2]); // 0 for float, 1 for double
+    int direction = atoi(argv[3]);
+    int target_level = atoi(argv[4]);
+    const int num_dims = atoi(argv[5]);
+    vector<size_t> dims(num_dims);
+    for(int i=0; i<dims.size(); i++){
+       dims[i] = atoi(argv[6 + i]);
+       cout << dims[i] << " ";
+    }
+    cout << endl;
+    switch(type){
+        case 0:
+            {
+                test<float>(filename, dims, direction, target_level);
+                break;
+            }
+        case 1:
+            {
+                test<double>(filename, dims, direction, target_level);
+                break;
+            }
+        default:
+            cerr << "Only 0 (float) and 1 (double) are implemented in this test\n";
+            exit(0);
+    }
+    return 0;
+}
